d87cc298585b17187a356bf44f2de1ce969b4baf
bachlex1608 <lxbachit03.working@gmail.com>
2025-05-23 14:12:03 +0700

    hello_world project

---
+++ hello_world/app/client.mjs
@@ -0,0 +1,65 @@
+import {
+  Connection,
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import { getKeypairFromFile } from "@solana-developers/helpers";
+
+const programId = new PublicKey("Ap6vYz56ZzDdmXvxyU6KMrZuPj8Yc1VjByiU1ng42qLF")
+console.log("Program ID: ", programId)
+console.log("Program ID: ", programId.toBase58())
+
+
+// Connect to a solana cluster. Either to your local test validator or to devnet
+const connection = new Connection("http://localhost:8899", "confirmed")
+//const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+console.log("Connection: ", connection)
+
+// We load the keypair that we created in a previous step
+const keyPair = await getKeypairFromFile("~/.config/solana/id.json");
+console.log("KeyPair: ", keyPair)
+console.log("KeyPair.publicKey: ", keyPair.publicKey.toBase58())
+console.log("KeyPair.secretKey: ", keyPair.secretKey.toString("base64"))
+
+// Every transaction requires a blockhash
+const blockhashInfo = await connection.getLatestBlockhash();
+console.log("Blockhash: ", blockhashInfo)
+
+// Create a new transaction
+const tx  = new Transaction({
+    ...blockhashInfo
+})
+console.log("Transaction: ", tx)
+
+
+// Add our Hello World instruction
+const instruction = new TransactionInstruction({
+    programId: programId, // The program ID deployed to the cluster
+    data: Buffer.from([]), // The instruction data, a instruction parameter that the program will use to perform its logic | Empty for now
+    keys: []
+}) 
+console.log("Instruction: ", instruction)
+
+tx.add(instruction);
+console.log("Transaction with instruction: ", tx)
+
+// Sign the transaction with your previously created keypair
+tx.sign(keyPair);
+console.log("Transaction with signature: ", tx)
+
+const txHash = await connection.sendRawTransaction(tx.serialize()); // signature
+console.log("Transaction sent with hash: ", txHash);
+
+// Confirm the transaction
+await connection.confirmTransaction({
+    blockhash: blockhashInfo.blockhash,
+    lastValidBlockHeight: blockhashInfo.lastValidBlockHeight,
+    signature: txHash,
+});
+
+
+console.log(
+  `Congratulations! Look at your ‘Hello World' transaction in the Solana Explorer:
+  https://explorer.solana.com/tx/${txHash}?cluster=custom`,
+);
--- hello_world/src/lib.rs
+++ hello_world/src/lib.rs
@@ -6,22 +6,20 @@ use solana_program::{
     msg,
 };
 
+// declare and export the program's entrypoint
+entrypoint!(process_instruction);
 
-pub fn process_instruction() -> ProgramResult {
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8]
+) -> ProgramResult {
+    // log a message to the blockchain
+    msg!("Hello, world!");
+
+    msg!("_instruction_data: {:?}", _instruction_data);
+    
+
+    // gracefully exit the program
     Ok(())
-}
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
